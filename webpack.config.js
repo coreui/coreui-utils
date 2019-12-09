f3bed4671d488b92786bfdd5878e2e075489bb6a
woothu <32914662+woothu@users.noreply.github.com>
2019-12-09 14:41:55 +0100

    chore: add package.json, add webpack and babel configuration

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'coreui-utils.common.js',
+    libraryTarget: 'commonjs'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }]
+  }
+}
