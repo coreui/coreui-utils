df9ccb147ea302cddd3e900bd1dcf00e5802a83e
mrholek <lukasz@holeczek.pl>
2023-05-14 21:40:15 +0200

    refactor: add SSR support

--- src/getColor.ts
+++ src/getColor.ts
@@ -7,7 +7,7 @@
 
 import getStyle from './getStyle'
 
-const getColor = (rawProperty: string, element = document.body) => {
+const getColor = (rawProperty: string, element?: Element) => {
   const property = `--${rawProperty}`
   const style = getStyle(property, element)
   return style ? style : rawProperty
--- src/getStyle.ts
+++ src/getStyle.ts
@@ -5,8 +5,18 @@
  * --------------------------------------------------------------------------
  */
 
-const getStyle = (property: string, element = document.body) => {
-  return window.getComputedStyle(element, null).getPropertyValue(property).replace(/^\s/, '')
+const getStyle = (property: string, element?: Element) => {
+  if (typeof window === 'undefined') {
+    return
+  }
+
+  if (typeof document === 'undefined') {
+    return
+  }
+
+  const _element = element ?? document.body
+
+  return window.getComputedStyle(_element, null).getPropertyValue(property).replace(/^\s/, '')
 }
 
 export default getStyle
